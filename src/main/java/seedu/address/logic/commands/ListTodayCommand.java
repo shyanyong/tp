8f6251354736097fe0523a96a7688bf468971991
shyanyong <wshyanyong123@gmail.com>
2023-10-13 21:02:13 +0800

    Add baseline for list today command

---
+++ src/main/java/seedu/address/logic/commands/ListTodayCommand.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.model.Model;
+
+/**
+ * Lists all remaining medications to be taken for the day to the user.
+ */
+public class ListTodayCommand extends Command {
+
+    public static final String COMMAND_WORD = "list today";
+
+    public static final String MESSAGE_SUCCESS = "Listing all medications to be taken today";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
+
+
