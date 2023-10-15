928b82e80d6ce44531d7b1c75c0df05af4c9fd2a
WX <119654395+RoeReRe@users.noreply.github.com>
2023-10-15 15:46:42 +0800

    Update ListPrescriptionCommandParser.java

--- src/main/java/seedu/address/logic/parser/ListPrescriptionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListPrescriptionCommandParser.java
@@ -8,13 +8,13 @@ import seedu.address.logic.commands.ListPrescriptionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new AddCommand object
+ * Parses input arguments and creates a new ListPrescriptionCommand object
  */
 public class ListPrescriptionCommandParser implements ParserPrescription<ListPrescriptionCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand
-     * and returns an AddCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the ListPrescriptionCommand
+     * and returns a ListPrescriptionCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
