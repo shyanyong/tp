11f245a81ec81b07509f78bed0efc26f8e048cb7
WX Game <weixiang05@gmail.com>
2023-10-15 13:59:53 +0800

    Fix slight style error

--- src/main/java/seedu/address/logic/commands/ListPrescriptionCommand.java
+++ src/main/java/seedu/address/logic/commands/ListPrescriptionCommand.java
@@ -13,7 +13,7 @@ public class ListPrescriptionCommand extends CommandPrescription {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all prescriptions stored.\n"
-        + "Example: " + COMMAND_WORD;
+            + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Listed all prescriptions";
 
