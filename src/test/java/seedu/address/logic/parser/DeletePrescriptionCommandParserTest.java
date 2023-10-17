3109be511b444db781e40a4e959c4647d287cd3d
Ren Jing <e0958325@u.nus.edu>
2023-10-17 09:50:04 +0800

    Test delete command parser

---
+++ src/test/java/seedu/address/logic/parser/DeletePrescriptionCommandParserTest.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
+import static seedu.address.logic.MessagesPrescription.MESSAGE_INVALID_COMMAND_FORMAT;
+import seedu.address.logic.commands.DeletePrescriptionCommand;
+import static seedu.address.logic.parser.CommandPrescriptionParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandPrescriptionParserTestUtil.assertParseSuccess;
+
+public class DeletePrescriptionCommandParserTest {
+    private DeletePrescriptionCommandParser parser = new DeletePrescriptionCommandParser();
+
+    //write some test cases for the parser
+    @Test
+    public void parse_validArgs_success() {
+        // Valid index
+        assertParseSuccess(parser, " i/1", new DeletePrescriptionCommand(Index.fromOneBased(1)));
+    }
+
+    @Test
+    public void parse_invalidArgs_failure() {
+        // Valid index
+        assertParseFailure(parser, " i/0", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeletePrescriptionCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyPreamble_failure() {
+        // No index specified
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeletePrescriptionCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_extraValues_failure() {
+        // Random values
+        assertParseFailure(parser, "ABCDEFGH",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        DeletePrescriptionCommand.MESSAGE_USAGE));
+
+        // Random prefixes
+        assertParseFailure(parser, "mn/ABCD d/2",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        DeletePrescriptionCommand.MESSAGE_USAGE));
+    }
+}
