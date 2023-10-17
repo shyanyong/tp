e14076f5b768ed1d23585af35d9eb72b61dfd168
jingyu987 <jingyu2987@gmail.com>
2023-10-18 01:48:36 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/TakePrescriptionCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/TakePrescriptionCommandParserTest.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.MessagesPrescription.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandPrescriptionParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandPrescriptionParserTestUtil.assertParseSuccess;
 import static seedu.address.logic.parser.CliSyntaxPrescription.PREFIX_CONSUMPTION;
 import static seedu.address.logic.parser.CliSyntaxPrescription.PREFIX_NAME;
+import static seedu.address.logic.parser.CommandPrescriptionParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandPrescriptionParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
