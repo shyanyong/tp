577069701b9c103ec97ce711913af059163d11c7
ChongWeiJie29 <chongweijie29@gmail.com>
2023-10-18 21:15:54 +0800

    Modify Prescription constructor to accept inputs without consumption count

--- src/main/java/seedu/address/model/prescription/Prescription.java
+++ src/main/java/seedu/address/model/prescription/Prescription.java
@@ -30,6 +30,12 @@ public class Prescription {
     private final Note note;
     // private final Set<Tag> tags = new HashSet<>();
 
+    public Prescription(Name name, Dosage dosage, Frequency frequency, Date startDate,
+                        Date endDate, Date expiryDate, Stock totalStock, Note note) {
+        this(name, dosage, frequency, startDate, endDate, expiryDate,
+            totalStock, new ConsumptionCount("0", false), note);
+    }
+
     /**
      * Every field must be present and not null.
      */
